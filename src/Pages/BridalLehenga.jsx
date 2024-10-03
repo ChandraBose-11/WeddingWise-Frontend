1d2ada948a3c94810fb6d0b5aca1d6d289fd0177
ChandraBose-11 <chandrubose46@gamil.com>
2024-10-03 08:33:39 +0530

    Completed

--- src/Pages/BridalLehenga.jsx
+++ src/Pages/BridalLehenga.jsx
@@ -10,32 +10,45 @@ import { Tooltip, TextInput } from "flowbite-react";
 import { Link } from "react-router-dom";
 
 const BridalLehenga = () => {
-    const [text] = useState("+91 1234567890");
+  const [text] = useState("+91 1234567890");
 
-    const [isOpen, setIsOpen] = useState(false);
-  
-    const toggleModal = () => {
-      setIsOpen(!isOpen);
-    };
-    return (
-        <div>
-            <div className="my-2 flex flex-col lg:flex-row w-full h-max max-w-full bg-red-100 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsOpen(!isOpen);
+  };
+  return (
+    <div>
+      <div className="my-2 flex flex-col lg:flex-row w-full h-max max-w-full bg-red-100 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         <Link to="#">
           <img
             className="p-8 rounded-t-lg h-96 max-w-96"
-            src=""
+            src="https://i.pinimg.com/736x/5e/1f/f5/5e1ff563b7a2177487c52cb2374885d2.jpg"
             alt="Wedding-Resorts"
           />
         </Link>
         <div className="px-5 pb-5 ">
           <Link to="#">
             <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white mt-7">
-             
+              Kimayann
             </h5>
           </Link>
           <br />
           <p>
-        
+            Kimayan is a designer store located in the city of Chennai. It is a
+            luxury brand, established by the creative husband and wife duo, Neha
+            and Pradeep Hirani. Established in 2002,Kimayan is now a market
+            leader in couture and print collections, well supported by good
+            merchandising, fine operational expertise, and excellent customer
+            management. It offers a wide collection of the latest in fashion by
+            countless designers and it is one of the biggest buyers at the
+            Indian Fashion Week. Experience Kimaya is a well-known designer
+            store. It gave a well-deserving jolt to the fashion retail business,
+            adding a much needed professional touch to a chaotic setup. Started
+            with a creative group of 26 leading designers, within six years it
+            features more than 140 Indian and Italian designers. It opened a new
+            store in Dubai, after a year in retail and now has a presence in all
+            the major cities of India.
           </p>
           <div className="flex items-center mt-24">
             <div className="flex items-center space-x-1 rtl:space-x-reverse">
@@ -92,7 +105,7 @@ const BridalLehenga = () => {
           <br />
           <div className="flex items-center justify-between">
             <span className="text-xl text-gray-900 font-bold dark:text-white">
-              From Rs.100000
+              From Bridal Wear ₹40,000
             </span>
 
             <Button onClick={toggleModal} className="sm:ml-80">
@@ -129,19 +142,32 @@ const BridalLehenga = () => {
         <Link to="#">
           <img
             className="p-8 rounded-t-lg h-96 max-w-96"
-            src=""
+            src="https://i.pinimg.com/originals/1a/03/3d/1a033d7dfb7ddbbec199a08664dafd4f.jpg"
             alt="Wedding-Resorts"
           />
         </Link>
         <div className="px-5 pb-5 ">
           <Link to="#">
             <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white mt-7">
-             
+              Montage Fashion House
             </h5>
           </Link>
           <br />
           <p>
-        
+            Montage Fashion House is a designer store located in Nungambakkam
+            which is situated in the city of Chennai. Every girl has an innate
+            desire to look fabulous and perfect in her Bridal Avatar and for
+            this to happen, everything you wear should be on-point. If you are
+            looking for a great designer store to get your bridal attire, then
+            the Montage Fashion House is the choice that you should make. It is
+            a one-stop shop which offers all types of western and ethnic wear
+            for brides and their bridesmaids as well. Designs offered The bride
+            needs to look beautiful and elegant for all the pre-wedding, wedding
+            and post-wedding functions and the bridal attire should match her
+            personality as well as the sense of style for all the ceremonies.
+            The creative team of designers at Montage Fashion House have been
+            designing stunning outfits ranging in different colours, for a very
+            long time and know exactly how to deliver what you demand.
           </p>
           <div className="flex items-center mt-24">
             <div className="flex items-center space-x-1 rtl:space-x-reverse">
@@ -198,7 +224,7 @@ const BridalLehenga = () => {
           <br />
           <div className="flex items-center justify-between">
             <span className="text-xl text-gray-900 font-bold dark:text-white">
-              From Rs.100000
+              From Bridal Wear ₹45,000
             </span>
 
             <Button onClick={toggleModal} className="sm:ml-80">
@@ -235,19 +261,31 @@ const BridalLehenga = () => {
         <Link to="#">
           <img
             className="p-8 rounded-t-lg h-96 max-w-96"
-            src=""
+            src="https://i.pinimg.com/736x/4e/6c/b7/4e6cb7678c09d1a2d7a63a4fb17707ef.jpg"
             alt="Wedding-Resorts"
           />
         </Link>
         <div className="px-5 pb-5 ">
           <Link to="#">
             <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white mt-7">
-             
+              Sionnah
             </h5>
           </Link>
           <br />
           <p>
-        
+            Sionnah is a bridal lehenga store based in Nungambakkam, Chennai.
+            Being one of the most sought-after couture brands based in the
+            metropolitan city of Chennai, they provide you with gorgeous options
+            when it comes to your bridal attire. You will certainly get many
+            beautiful dresses of your choice and it will be hard to choose a few
+            so to not overstuff your closets. So, don't waste your time in
+            thinking and head out to this store to start shopping. Designs
+            Offered There are quite many ceremonies that happen before and after
+            the main wedding ceremony and each of it demands new apparel for the
+            bride to look the best and stand apart from the crowd. Keeping this
+            in mind, Sionnah creates many gorgeous designs to fulfil its
+            clients' need. They have a variety of shades and patterns to satiate
+            your inner fashionista.
           </p>
           <div className="flex items-center mt-24">
             <div className="flex items-center space-x-1 rtl:space-x-reverse">
@@ -304,7 +342,7 @@ const BridalLehenga = () => {
           <br />
           <div className="flex items-center justify-between">
             <span className="text-xl text-gray-900 font-bold dark:text-white">
-              From Rs.100000
+              From Bridal Wear ₹10,000
             </span>
 
             <Button onClick={toggleModal} className="sm:ml-80">
@@ -341,19 +379,41 @@ const BridalLehenga = () => {
         <Link to="#">
           <img
             className="p-8 rounded-t-lg h-96 max-w-96"
-            src=""
+            src="https://i.pinimg.com/736x/af/15/46/af1546f69c986a1775133a62141042dc.jpg"
             alt="Wedding-Resorts"
           />
         </Link>
         <div className="px-5 pb-5 ">
           <Link to="#">
             <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white mt-7">
-             
+              Label Ritu Kumar, Chennai
             </h5>
           </Link>
           <br />
           <p>
-        
+            Label Ritu Kumar is a high-end couture brand located in Chennai and
+            deals with an array of outfit options for every occasion and
+            festivity. If you want to wear a lehenga that you always longed for,
+            they will have it ready for you. The array of designs that the
+            company offers starts from quirky printed Into-western outfits to
+            gorgeous bridal lehengas and trousseau wear. The brand is known for
+            its rich and distinctive use of colours, exquisite embroideries, and
+            outfits which are rich in Indian design. Every outfit that is
+            offered by Label Ritu Kumar is a marvellous piece. Experience Since
+            its inception in 1969, the brand that was started by Label Ritu
+            Kumar has grown in popularity to reach new heights. The designs
+            reflect her unique style that is a blend of ancient ethnic tradition
+            with a contemporary vibe to it. She has dressed every well-known
+            celebrity and walked for all the celebrated fashion weeks of the
+            country. Rest assured as she will ensure to make you look phenomenal
+            in your bridal avatar. Designs Offered It is not a shocker that the
+            brand provides innumerable designs for you to opt for on your
+            special day. The talented team of designers who work with Label Ritu
+            Kumar also customise your outfits in a way you want it to look like.
+            A few of the many designs that they offer for bridal wear are
+            lehengas, beautiful sarees, and stunning Indo-western outfits. Be
+            ready to look at your jaw-dropping best by choosing them as your
+            designer wear for your wedding and pre-wedding events.
           </p>
           <div className="flex items-center mt-24">
             <div className="flex items-center space-x-1 rtl:space-x-reverse">
@@ -410,7 +470,7 @@ const BridalLehenga = () => {
           <br />
           <div className="flex items-center justify-between">
             <span className="text-xl text-gray-900 font-bold dark:text-white">
-              From Rs.100000
+              From Bridal Wear ₹2,5000
             </span>
 
             <Button onClick={toggleModal} className="sm:ml-80">
@@ -447,19 +507,38 @@ const BridalLehenga = () => {
         <Link to="#">
           <img
             className="p-8 rounded-t-lg h-96 max-w-96"
-            src=""
+            src="https://i.pinimg.com/236x/a4/11/d5/a411d571111b9906032b8fd62f16d1b5.jpg"
             alt="Wedding-Resorts"
           />
         </Link>
         <div className="px-5 pb-5 ">
           <Link to="#">
             <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white mt-7">
-             
+              F2 Designer Boutique
             </h5>
           </Link>
           <br />
           <p>
-        
+            F2 Designer Boutique is a well-famed bridal lehenga designer nestled
+            in the heart of Ramapuram, Chennai. Renowned for its exquisite
+            bridal lehengas, the F2 Designer Boutique is your go-to destination
+            for capturing the essence of traditional charm and contemporary
+            allure. The F2F team ensures that each bridal lehenga is a
+            masterpiece, meticulously crafted to perfection. From intricate
+            embroidery to luxurious fabrics, every detail reflects the
+            dedication to creating a garment as unique as the bride herself. The
+            boutique takes pride in offering a diverse collection that caters to
+            individual tastes and preferences, ensuring that every bride finds
+            her dream ensemble. What F2 Designer Boutique brings to the table F2
+            Designer Boutique offers personalised consultations to bring your
+            vision to life. The skilled artisans and designers work tirelessly
+            to make your bridal dreams a reality, ensuring you radiate
+            confidence and grace on your special day. They also provide rental
+            bridal wear options and other party wear dresses for weddings. They
+            are expert in designing and offering bespoke bridal lehenga that
+            works magic for your dreamy bridal look. From gowns to indo-western
+            bridal attires, F2 Designer Boutique has something for all bridal
+            garment needs.
           </p>
           <div className="flex items-center mt-24">
             <div className="flex items-center space-x-1 rtl:space-x-reverse">
@@ -516,7 +595,7 @@ const BridalLehenga = () => {
           <br />
           <div className="flex items-center justify-between">
             <span className="text-xl text-gray-900 font-bold dark:text-white">
-              From Rs.100000
+              From Bridal Wear ₹15,000
             </span>
 
             <Button onClick={toggleModal} className="sm:ml-80">
@@ -549,10 +628,8 @@ const BridalLehenga = () => {
           </div>
         </div>
       </div>
-        </div>
-    );
+    </div>
+  );
 };
 
 export default BridalLehenga;
-
-
--- src/Pages/Sherwani.jsx
+++ src/Pages/Sherwani.jsx
@@ -10,32 +10,48 @@ import { Tooltip, TextInput } from "flowbite-react";
 import { Link } from "react-router-dom";
 
 const Sherwani = () => {
-    const [text] = useState("+91 1234567890");
+  const [text] = useState("+91 1234567890");
 
-    const [isOpen, setIsOpen] = useState(false);
-  
-    const toggleModal = () => {
-      setIsOpen(!isOpen);
-    };
-    return (
-        <div>
-            <div className="my-2 flex flex-col lg:flex-row w-full h-max max-w-full bg-red-100 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsOpen(!isOpen);
+  };
+  return (
+    <div>
+      <div className="my-2 flex flex-col lg:flex-row w-full h-max max-w-full bg-red-100 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         <Link to="#">
           <img
             className="p-8 rounded-t-lg h-96 max-w-96"
-            src=""
-            alt="Wedding-Resorts"
+            src="https://i.pinimg.com/736x/97/1b/5f/971b5fc6ba878dff67e557cb52411ce5.jpg"
+            alt="Sherwani"
           />
         </Link>
         <div className="px-5 pb-5 ">
           <Link to="#">
             <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white mt-7">
-             
+              Peter England
             </h5>
           </Link>
           <br />
           <p>
-        
+            Peter England is a retail store located in the city of Chennai.
+            Originally the brand was founded to provide British soldiers with
+            fine clothing during a war but soon found its way to Indian markets
+            providing the exquisite of the menswear that you can choose to
+            flaunt on your special occasions. It has been among the top menswear
+            brands in the country holding its reputation by providing products
+            and services that are not only best in quality but a class apart.
+            With a staunch belief in authenticity, it has become one of the most
+            trusted brands that offer and adds value and charm to groomswear.
+            Designs Offered Peter England offers innovative and regal designs
+            that you cannot resist flaunting on your wedding or any of your
+            special occasions, while you can be sure that all the eyes would be
+            on you. With over 700 outlets over the country, they design clothes
+            as per your needs and offer you a collection from which you would
+            surely find your perfect wedding attire. To ensure that all of their
+            clients have a wholesome shopping experience, they can even offer
+            you domestic shipping services.
           </p>
           <div className="flex items-center mt-24">
             <div className="flex items-center space-x-1 rtl:space-x-reverse">
@@ -92,7 +108,7 @@ const Sherwani = () => {
           <br />
           <div className="flex items-center justify-between">
             <span className="text-xl text-gray-900 font-bold dark:text-white">
-              From Rs.100000
+              From Sherwani ₹10,000
             </span>
 
             <Button onClick={toggleModal} className="sm:ml-80">
@@ -129,19 +145,36 @@ const Sherwani = () => {
         <Link to="#">
           <img
             className="p-8 rounded-t-lg h-96 max-w-96"
-            src=""
-            alt="Wedding-Resorts"
+            src="https://i.pinimg.com/736x/4f/f5/bc/4ff5bc113e590add72844c844f383afa.jpg"
+            alt="Sherwani"
           />
         </Link>
         <div className="px-5 pb-5 ">
           <Link to="#">
             <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white mt-7">
-             
+              Raymond Shop
             </h5>
           </Link>
           <br />
           <p>
-        
+            Raymond Shop is a men's clothing retails store located in Chennai
+            and offers a complete solution to a men’s wardrobe from fabric to
+            readymade garments to custom-tailoring. For over nine decades brand
+            continues to be one of the leading premium fashion destination in
+            India aspired by all generations. The Raymond Shop is a one-stop
+            destination for men’s fashion lifestyle. You can shop from an
+            extensive men’s range from formal to ceremonial and social to casual
+            collections. With a dedicated section for accessories, one can
+            choose from a wide collection of ties, belts, bows, cuff links and
+            pocket squares and much more to complete the look. Designs Offered
+            The Raymond Shop offers a wide array of fashion clothing options in
+            suits, jackets, shirts and trousers for discerning customers through
+            an exquisite range of Raymond's fine fabrics along with ready to
+            wear apparel, accessories and custom tailoring services. With more
+            than 1000 stores, it is the largest one-stop fashion lifestyle
+            destination in the country. They produce 20,000 designs for suiting
+            and clothing, and you can find anything for your friends and family
+            as well.
           </p>
           <div className="flex items-center mt-24">
             <div className="flex items-center space-x-1 rtl:space-x-reverse">
@@ -198,7 +231,7 @@ const Sherwani = () => {
           <br />
           <div className="flex items-center justify-between">
             <span className="text-xl text-gray-900 font-bold dark:text-white">
-              From Rs.100000
+              From Sherwani ₹5,000
             </span>
 
             <Button onClick={toggleModal} className="sm:ml-80">
@@ -235,19 +268,32 @@ const Sherwani = () => {
         <Link to="#">
           <img
             className="p-8 rounded-t-lg h-96 max-w-96"
-            src=""
-            alt="Wedding-Resorts"
+            src="https://i.pinimg.com/236x/43/1c/e4/431ce47c95f2a9bc962f7c1bb8cd3741.jpg"
+            alt="Sherwani"
           />
         </Link>
         <div className="px-5 pb-5 ">
           <Link to="#">
             <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white mt-7">
-             
+              Aravind Store,
             </h5>
           </Link>
           <br />
           <p>
-        
+            The AravindStore is a menswear retail store located in Annanagar,
+            Chennai. It aims to create an enriching shopping experience through
+            the convergence of its strongest competencies under one roof. The
+            store not only has the entire range of shirting and suiting fabrics
+            but also offers super-premium designs for the discerning customer.
+            The suiting collection in The AravindStore consists of new-age
+            fabrics that are perfectly suited for any of your wedding related
+            functions and events. Designs Offered Be it any of the pre-wedding,
+            wedding or post-wedding functions, being the groom you need need to
+            put your best self forward for all your ceremonies as all the eyes
+            are on you for even the minute details. They, at The AravindStore,
+            offer a whole variety of different patterns, colours, designs and
+            collections for you to choose as per what suits you the best and
+            ensure that you look awesome for your nuptial ceremonies.
           </p>
           <div className="flex items-center mt-24">
             <div className="flex items-center space-x-1 rtl:space-x-reverse">
@@ -304,7 +350,7 @@ const Sherwani = () => {
           <br />
           <div className="flex items-center justify-between">
             <span className="text-xl text-gray-900 font-bold dark:text-white">
-              From Rs.100000
+              From Sherwani ₹2,499
             </span>
 
             <Button onClick={toggleModal} className="sm:ml-80">
@@ -341,19 +387,35 @@ const Sherwani = () => {
         <Link to="#">
           <img
             className="p-8 rounded-t-lg h-96 max-w-96"
-            src=""
-            alt="Wedding-Resorts"
+            src="https://i.pinimg.com/736x/4b/9e/69/4b9e6998de56637ea987e30512aac8df.jpg"
+            alt="Sherwani"
           />
         </Link>
         <div className="px-5 pb-5 ">
           <Link to="#">
             <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white mt-7">
-             
+              Jain Collection
             </h5>
           </Link>
           <br />
           <p>
-        
+            Jain Collection, Kilpauk is a chain of men's clothing store located
+            in the city of Chennai and is one of the most reliable brands for
+            men's ethnic wear. When it comes to weddings, there are a lot of
+            functions to attend and being the groom, you need to look your best
+            at all times. Specialising in both, contemporary and traditional,
+            styles, Jain Collection, Kilpauk offers a wide array of outfits,
+            which will make you look mindblowing for your occasion. Their store
+            which has wedding sherwanis, suits, and blazers for the groom as
+            well as his family. Designs offered The groom needs to look handsome
+            and elegant for all the pre-wedding, wedding and post-wedding
+            functions and the wedding attire plays a very important role in this
+            to turn into reality. Jain Collection, Kilpauk uses different and
+            unique embroideries, colours and designs to make an outfit for you
+            which truly compliments your personality and suits your sense of
+            style. They also offer an option for domestic shipping, at an
+            additional cost, so that you can get your outfits as per your
+            convenience.
           </p>
           <div className="flex items-center mt-24">
             <div className="flex items-center space-x-1 rtl:space-x-reverse">
@@ -410,7 +472,7 @@ const Sherwani = () => {
           <br />
           <div className="flex items-center justify-between">
             <span className="text-xl text-gray-900 font-bold dark:text-white">
-              From Rs.100000
+              From Sherwani ₹10,000
             </span>
 
             <Button onClick={toggleModal} className="sm:ml-80">
@@ -447,19 +509,33 @@ const Sherwani = () => {
         <Link to="#">
           <img
             className="p-8 rounded-t-lg h-96 max-w-96"
-            src=""
-            alt="Wedding-Resorts"
+            src="https://i.pinimg.com/736x/cd/db/f9/cddbf91b5e158aef21f0403f01b1f448.jpg"
+            alt="Sherwani"
           />
         </Link>
         <div className="px-5 pb-5 ">
           <Link to="#">
             <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white mt-7">
-             
+              Swayamvar
             </h5>
           </Link>
           <br />
           <p>
-        
+            Swayamvar is a wedding sherwani shop that has a wide display of all
+            the wedding-related attires for the groom. Weddings and sherwanis go
+            hand in hand and every groom needs an outfit that he can flaunt for
+            the grand night and for this to come true he can always reach out to
+            The Swayamvar store. They have a wide range of fashion wear for men
+            who like to walk down the aisle in style and have all the guests
+            turn their heads to have one look at them and not just the bride.
+            Designs Offered They have an exhibit of designs that they offer to
+            the clients and make them happy and confident about the look that
+            they are about to carry. The Swayamvar has a collection that
+            includes designs like indo-western, totally traditional, and western
+            and makes sure you have wide choices in every shade and pattern.
+            They will ensure that all your fashion needs are fulfilled and you
+            the best attire for your grand wedding ceremony. They even let you
+            customise your attire for a perfect fit.
           </p>
           <div className="flex items-center mt-24">
             <div className="flex items-center space-x-1 rtl:space-x-reverse">
@@ -516,7 +592,7 @@ const Sherwani = () => {
           <br />
           <div className="flex items-center justify-between">
             <span className="text-xl text-gray-900 font-bold dark:text-white">
-              From Rs.100000
+              From Sherwani ₹15,000
             </span>
 
             <Button onClick={toggleModal} className="sm:ml-80">
@@ -549,10 +625,8 @@ const Sherwani = () => {
           </div>
         </div>
       </div>
-        </div>
-    );
+    </div>
+  );
 };
 
 export default Sherwani;
-
-
